4926a4e5f3302af2962d6f56c4b6d3e2acbb2fe6
Erkberg <erisch01@web.de>
2018-12-01 22:09:23 +0100

    ground generation

---
+++ Sacrizism/Assets/Scripts/Editor/WorldGeneration.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+public class WorldGeneration
+{
+    [MenuItem("Sacrizism/WorldCreation/CreateGround")]
+    public static void CreateGround()
+    {
+        GameObject.FindObjectOfType<WorldManager>().CreateGround();
+    }
+}
+#endif
---
+++ Sacrizism/Assets/Scripts/General/WorldManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class WorldManager : MonoBehaviour
+{
+    private const int WorldSize = 2;
+    private const float TileSize = 10.24f;
+
+    public Transform groundHolder;
+
+    public Transform groundTilePrefab;
+
+    public void CreateWorld()
+    {
+        CreateGround();
+    }
+
+    public void CreateGround()
+    {
+        for (int i = -WorldSize; i <= WorldSize; i++)
+        {
+            for (int j = -WorldSize; j <= WorldSize; j++)
+            {
+#if UNITY_EDITOR
+                Transform clone = PrefabUtility.InstantiatePrefab(groundTilePrefab as Transform) as Transform;
+                clone.transform.position = new Vector3(i * TileSize, j * TileSize, 0f);
+                clone.name = "GroundTile_" + i + "_" + j;
+                clone.transform.parent = groundHolder;
+#else
+                Instantiate(groundTilePrefab, new Vector3(i * TileSize, j * TileSize, 0f), Quaternion.identity, groundHolder);
+#endif
+            }
+        }
+    }
+}
