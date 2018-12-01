141ba2d334defa6e39b026f86c0899d165940343
Erkberg <erisch01@web.de>
2018-12-01 22:28:16 +0100

    basic player movement and camera follow

---
+++ Sacrizism/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public Transform mainCam;
+
+    private float followSmoothTime = 0.1f;
+
+    private Vector3 refVelocity;
+    private Vector3 camToPlayerOffset = new Vector3(0f, 0f, -10f);
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.position + camToPlayerOffset, ref refVelocity, followSmoothTime);
+    }
+}
